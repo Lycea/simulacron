b3f498ac9ea8bfd7136772f1ff4ccd12de1d18e5
Lycea <20510874+Lycea@users.noreply.github.com>
2022-07-21 22:38:23 +0200

    adjusted chop movement to search a new tree if we did not collect any wood on wood down

--- components/tasks/chop.lua
+++ components/tasks/chop.lua
@@ -4,9 +4,20 @@ function chop:new(parent)
     self.parent = parent
     self.chop_target = nil
     self.wood = 0
+
+    self.move_back = false
 end
 
 function chop:update()
+    if self.move_back then
+        if self.parent.mover:done() then
+            self.parent.villager.village:add_resource("wood", self.wood)
+            return true
+        else
+            return 
+        end
+    end
+
     --print("update chop...")
     if self.chop_target == nil then
         table.insert(self.parent.villager.tasks,
@@ -16,9 +27,13 @@ function chop:update()
     else
         local wood = self.chop_target.tree:chop()
         --tree is done
-        if wood == 0 then
-            self.parent.villager.village:add_resource("wood", self.wood)
-            return true
+        if wood == 0 and self.wood ~= 0 then
+            --self.parent.villager.village:add_resource("wood", self.wood)
+
+            self.parent.mover:set_goal(self.parent.villager.village.pos)
+            self.move_back = true
+        elseif wood == 0 and self.wood == 0  then
+            self.chop_target = nil
         end
         self.wood = self.wood + wood
     end
